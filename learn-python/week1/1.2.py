b781bd6c6a305c0163386d893974b6299e3330fe
Yexiaomo <markdownpad@163.com>
2017-07-24 19:53:27 +0800

    week1

---
+++ learn-python/week1/1.2.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+sum = 0
+tmp = 1
+'''上面可以写成一行
+sum, tmp = 0, 1
+'''
+for i in range(1,11):
+	tmp = 1
+	for j in range(1,i+1):
+		tmp *= j
+	sum += tmp;
+print('1!+2!+...+10! =',sum)
