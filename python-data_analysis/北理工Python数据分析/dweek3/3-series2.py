ddbe8786f6a49a75f4ec90f29eae25507fd6cfab
Yexiaomo <markdownpad@163.com>
2018-06-26 18:59:48 +0800

    整理目录2

--- python-data_analysis/北理工Python数据分析/dweek3/3-series2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-import pandas as pd
-
-d1 = pd.Series([9, 8, 7, 6])
-print('d1:\n', d1)
-
-d2 = pd.Series([9, 8, 7, 6], index=['a', 'b', 'c', 'd'])
-print('d2:\n', d2)
